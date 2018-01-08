bf84187201766dd9db3cbc6313809fc112eacda1
Anton Skshidlevsky <meefik@gmail.com>
2018-01-08 13:38:50 +0300

    Added alias su on sudo for current user

--- include/core/sudo/deploy.sh
+++ include/core/sudo/deploy.sh
@@ -6,11 +6,14 @@ do_configure()
 {
     msg ":: Configuring ${COMPONENT} ... "
     local sudo_str="${USER_NAME} ALL=(ALL:ALL) NOPASSWD:ALL"
-    if ! $(grep -q "${sudo_str}" "${CHROOT_DIR}/etc/sudoers"); then
+    if ! grep -q "${sudo_str}" "${CHROOT_DIR}/etc/sudoers"; then
         chmod 640 "${CHROOT_DIR}/etc/sudoers"
         echo ${sudo_str} >> "${CHROOT_DIR}/etc/sudoers"
         chmod 440 "${CHROOT_DIR}/etc/sudoers"
     fi
+    if [ -e "${CHROOT_DIR}/etc/profile.d" ]; then
+        echo '[ -n "$PS1" -a "$(whoami)" = "'${USER_NAME}'" ] || return 0' > "${CHROOT_DIR}/etc/profile.d/sudo.sh"
+        echo 'alias su="sudo -s"' >> "${CHROOT_DIR}/etc/profile.d/sudo.sh"
+    fi
     return 0
 }
-
